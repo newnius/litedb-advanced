61ce4cf8d8539f6aa16e79a3c07506fbc61bce12
GodWen <wucw5513@mails.jlu.edu.cn>
2015-11-16 16:45:25 +0800

    11-16

--- src/com/litedbAdvanced/disk/Main.java
+++ src/com/litedbAdvanced/disk/Main.java
@@ -1,24 +1,48 @@
 package com.litedbAdvanced.disk;
 
+import java.io.File;
+
+import com.litedbAdvanced.global.Line;
+import com.litedbAdvanced.global.TableDef;
+
 public class Main {
 	
+	Main(){
+		
+		LRUCache cache=new LRUCache(1000);
+		
+	}
+	
 	//创建文件并保存数据字典
-	static boolean createTable(byte[] tableName, TableDef tabelDef){}
+	public static boolean createTable(byte[] tableName, TableDef tabelDef){
+		
+		String path;
+		File fd;
+		
+		
+		
+		
+		
+	return true;}
+	
 	
 	//删除表相关所属的所有文件
-	static boolean deleteTable(byte[] tableName){}
+	public static boolean deleteTable(byte[] tableName){return true;}
 	
 	//根据 lineId 返回行数据
-	static Line loadRow(int lineId){}
+	public static Line loadRow(int lineId){
+		return null;}
 	
 	//更新行数据
-	static boolean updateRow(int lineId, Line updatedRow){}
+	public static boolean updateRow(int lineId, Line updatedRow){return true;}
 	
 	//删除行数据
-	static boolean deteteRow(int lineId){}
+	public static boolean deteteRow(int lineId){return true;}
 	
 	//添加行  
-	static boolean insertRow(Line newRow){}
+	public static boolean insertRow(Line newRow){return true;}
+	
+	
 	
 	public static void main(String[] args){
 		
